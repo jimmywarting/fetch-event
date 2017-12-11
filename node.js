e019999153073911e29e187ae1d9d24bc773fd45
Jimmy Wärting <jimmy@warting.se>
2017-12-11 18:16:40 +0100

    formating

--- node.js
+++ node.js
@@ -4,29 +4,30 @@ function fetchDecorator(nodeFetch) {
   let lib = nodeFetch
   let {Request} = lib
   let {fetch = lib} = lib
-
-	lib.fetch = (...args) => {
-		let request = new Request(...args)
-		let event = new FetchEvent({request})
+  
+  lib.fetch = (...args) => {
+    let request = new Request(...args)
+    let event = new FetchEvent({request})
     lib.fetch.emit('fetch', event)
-		return event.response || fetch(event.request)
-	}
+    return event.response || fetch(event.request)
+  }
 
   Object.setPrototypeOf(lib.fetch, new EventEmitter)
+  
   return lib
 }
 
 class FetchEvent {
-	constructor(init) {
-		this.type = 'fetch'
-		this.request = init.request
-		this.clientId = init.clientId || process.pid
-		this.isReload = init.isReload || false
-	}
-
-	respondWith(response) {
-		this.response = Promise.resolve(response)
-	}
+  constructor(init) {
+    this.type = 'fetch'
+    this.request = init.request
+    this.clientId = init.clientId || process.pid
+    this.isReload = init.isReload || false
+  }
+  
+  respondWith(response) {
+    this.response = Promise.resolve(response)
+  }
 }
 
 module.exports = fetchDecorator
